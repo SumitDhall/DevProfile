e8037bc171b75d5919e971bbf3c9dfa4061ad74a
SumitDhall <sumitdhall123@gmail.com>
2023-03-26 21:39:23 +0100

    rounded image corners

--- app/src/main/java/com/sumit/devprofile/MainActivity.kt
+++ app/src/main/java/com/sumit/devprofile/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.sumit.devprofile
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+//        rounded.isCircular = true
+        (findViewById<ImageView>(R.id.devSlopesLogo)).setImageDrawable(rounded)
     }
 }
